a59e4b28441e383742e554435212996e0f501c2e
LuxerIThink <luxerthegamer@gmail.com>
2023-05-27 01:15:59 +0200

    Fix train dataset path

--- processing/trainers.py
+++ processing/trainers.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class ModelTrainer:
     def __init__(self):
-        self.train_csv_path = f'{os.path.dirname(__file__)}/test/dataset/test_data.csv'
+        self.train_csv_path = f'{os.path.dirname(__file__)}/test/dataset/train_data.csv'
         self.rm_pattern = r"^Unnamed: 0|world_landmark_\d+\.[xyz]$"
 
     def run(self, test_csv_path: str):
@@ -15,7 +15,6 @@ class ModelTrainer:
         train_data = self.rm_cols(train_data)
         test_data = self.rm_cols(test_data)
 
-        pass
 
     def rm_cols(self, data: pd.DataFrame) -> pd.DataFrame:
         cols_to_rm = [col for col in data.columns if re.search(self.rm_pattern, col)]
