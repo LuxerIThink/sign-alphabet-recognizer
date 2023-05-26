92364e2e8241240c2d212339e52997efa5b6353d
LuxerIThink <luxerthegamer@gmail.com>
2023-05-27 00:29:24 +0200

    Add read csv files

--- processing/trainers.py
+++ processing/trainers.py
@@ -1,3 +1,12 @@
+import os
+import pandas as pd
+
+
 class ModelTrainer:
+    def __init__(self):
+        self.train_csv_path = f'{os.path.dirname(__file__)}/test/dataset/test_data.csv'
+
     def run(self, test_csv_path: str):
+        train_data = pd.read_csv(self.train_csv_path)
+        test_data = pd.read_csv(test_csv_path)
         pass
