0820244b7d1100812d7e0fb33c4cc88a641e53ff
LuxerIThink <luxerthegamer@gmail.com>
2023-05-27 00:30:06 +0200

    Fix run arguments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from processing.trainers import ModelTrainer
 
 if __name__ == '__main__':
-    test_csv_path = sys.argv[0]
-    output_csv_path = sys.argv[1]
+    test_csv_path = sys.argv[1]
+    output_csv_path = sys.argv[2]
     trainer = ModelTrainer()
     pred_solution = trainer.run(test_csv_path)
