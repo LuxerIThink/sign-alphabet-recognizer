d1fc1ddfc4bccc4825ed8935a66ffb06a2a0a00d
LuxerIThink <luxerthegamer@gmail.com>
2023-05-27 14:37:51 +0200

    Add cammand to main to show statistics

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     test_csv_path = sys.argv[1]
     output_csv_path = sys.argv[2]
     trainer = ModelTrainer()
-    pred_solution = trainer.run(test_csv_path)
+    y_pred_labels, y_test_decoded = trainer.run(test_csv_path)
+    # trainer.check_score(y_test_decoded, y_pred_labels)
+    # trainer.plot_conf_matrix(y_pred_labels, y_test_decoded)
