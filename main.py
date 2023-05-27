8a645370dd8165ca59fe2fd79482a7ad49c9f228
LuxerIThink <luxerthegamer@gmail.com>
2023-05-27 21:14:12 +0200

    Add save to file function

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import sys
 from processing.trainers import ModelTrainer
+import numpy as np
+
+
+def save_file(data: np.ndarray):
+    with open('output.txt', 'w') as file:
+        file.write('\n'.join(data))
+
 
 if __name__ == '__main__':
     test_csv_path = sys.argv[1]
@@ -8,3 +15,4 @@ if __name__ == '__main__':
     y_pred_labels, y_test_decoded = trainer.run(test_csv_path)
     # trainer.check_score(y_test_decoded, y_pred_labels)
     # trainer.plot_conf_matrix(y_pred_labels, y_test_decoded)
+    save_file(y_pred_labels)
