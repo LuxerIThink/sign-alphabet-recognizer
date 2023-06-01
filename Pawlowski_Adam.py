7214b4fde3064215e8aceee7dbf9e6a28a3c99f4
LuxerIThink <eliteluxer@gmail.com>
2023-06-01 02:03:23 +0200

    Fix output path

--- Pawlowski_Adam.py
+++ Pawlowski_Adam.py
@@ -3,8 +3,8 @@ from processing.predictors import SignAlphabetPredictor
 import numpy as np
 
 
-def save_file(data: np.ndarray):
-    with open('output.txt', 'w') as file:
+def save_file(data: np.ndarray, output_csv_path: str):
+    with open(output_csv_path, 'w') as file:
         file.write('\n'.join(data))
 
 
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     output_csv_path = sys.argv[2]
     trainer = SignAlphabetPredictor()
     y_pred_labels = trainer.run(test_csv_path)
-    save_file(y_pred_labels)
+    save_file(y_pred_labels, output_csv_path)
